7dcb8d6c591e619c29bd4c9e16cfb72deaa02b5a
Sean O'Leary <seamicole@gmail.com>
2024-11-17 02:50:56 -1000

    Create clone script

---
+++ clone.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the absolute path of the script
+SCRIPT_PATH=$(realpath "$0")
+
+# Create a directory for DeckFusion
+mkdir -p cloned-deckfusion
+
+# Enter DeckFusion directory
+cd cloned-deckfusion
+
+# Clone DeckFusion development repo
+git clone git@github.com:cloned-deckfusion/deckfusion.git
+
+# Clone DeckFusion landing repo
+git clone git@github.com:cloned-deckfusion/deckfusion-landing.git
+
+# Enter development repo
+cd deckfusion
+
+# Make project
+make
+
+# Delete clone script
+rm -- "$SCRIPT_PATH"
